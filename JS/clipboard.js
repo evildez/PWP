04fca50dfb78e0568bffc78361da274d22c656ed
Emilio Villarruel Fernandez <fernandezemiliov@gmail.com>
2019-11-03 12:01:14 -0600

    Test 1 for SVG

--- JS/clipboard.js
+++ JS/clipboard.js
@@ -1,7 +1,7 @@
 function copyCB() {
     var copyText = "fernandezemiliov@gmail.com";
     navigator.clipboard.writeText(copyText);
-    var notif = UIkit.notification("Copied " + copyText + " to clipboard.", {
+    var notif = UIkit.notification("Copied '"  + copyText + "' to clipboard.", {
         status: "success",
         pos: "bottom-center"
     });
